6f42dac8103f8da9dba8e63b7d77bd52a863bf2e
Jonathan Guymont <j.guymont@gmail.com>
2019-01-20 15:27:16 -0500

    add arparser

--- main.py
+++ main.py
@@ -1,61 +1,86 @@
+import os
+import argparse
 import torch
-from torchvision import transforms
-import torch.nn as nn
 from torch.utils.data import DataLoader
-from tqdm import tqdm
-from PIL import Image
+from torchvision import transforms
 from utils.data import SVHNDataset
 from utils import visualization
-from models.cnn import ConvNet
+from trainer import Trainer
 
-TRAIN_METADATA_PATH = './data/SVHN/train_metadata.pkl'
-TRAIN_DATA_DIR = './data/SVHN/train'
+def argparser():
+    """
+    Command line argument parser
+    """
+    parser = argparse.ArgumentParser(description='Split metadata into train/valid/test')
+    parser.add_argument('--train_metadata_path', type=str, default='./data/SVHN/metadata/train_metadata.pkl')
+    parser.add_argument('--valid_metadata_path', type=str, default='./data/SVHN/metadata/valid_metadata.pkl')
+    parser.add_argument('--test_metadata_path', type=str, default='./data/SVHN/metadata/test_metadata.pkl')
+    parser.add_argument('--data_dir', type=str, default='./data/SVHN/train')
+    parser.add_argument('--train_pct', type=float, default=0.7)
+    parser.add_argument('--valid_pct', type=float, default=0.2)
+    parser.add_argument('--test_pct', type=float, default=0.1)
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
-# Hyper parameters
-# TODO: Put in a config file
-num_epochs = 5
-num_classes = 5
-batch_size = 100
-learning_rate = 0.001
+    parser.add_argument('--model', type=str, choices=['ModelPaper', 'ConvNet'])
+    parser.add_argument('--num_classes', type=int, default=5)
+    parser.add_argument('--n_epochs', type=int, default=2)
+    parser.add_argument('--batch_size', type=int, default=100)
+    parser.add_argument('--learning_rate', type=float, default=0.001)
+    parser.add_argument('--crop_percent', type=float, default=0.3)
+    parser.add_argument('--optim', type=str, default='SGD')
+    parser.add_argument('--device', type=str, default='cpu')
+    return parser.parse_args()
 
 if __name__ == '__main__':
+    args = argparser()
     
+    args.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
     train_transforms = transforms.Compose([
-        transforms.Resize((64, 64)),
+        transforms.CenterCrop(64),
+        transforms.RandomApply([
+            transforms.RandomAffine(degrees=30, shear=20),
+            transforms.ColorJitter(brightness=0.5, contrast=.5, saturation=.5),
+            transforms.RandomRotation(20),
+        ], p=0.5),
+        transforms.RandomResizedCrop(54),
+        transforms.ToTensor(),
+        # transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+    ])
+
+    test_transforms = transforms.Compose([
+        transforms.CenterCrop(54),
         transforms.ToTensor(),
     ])
 
-    train_data = SVHNDataset(metadata_path=TRAIN_METADATA_PATH, data_dir=TRAIN_DATA_DIR, transform=train_transforms)
-    trainloader = DataLoader(train_data, batch_size=32, shuffle=True, num_workers=4, pin_memory=True)
+    train_data = SVHNDataset(
+        metadata_path=args.train_metadata_path, 
+        data_dir=args.data_dir, 
+        crop_percent=args.crop_percent, 
+        transform=train_transforms)
+
+    valid_data = SVHNDataset(
+        metadata_path=args.valid_metadata_path, 
+        data_dir=args.data_dir, 
+        crop_percent=args.crop_percent, 
+        transform=test_transforms)
     
+    test_data = SVHNDataset(
+        metadata_path=args.test_metadata_path, 
+        data_dir=args.data_dir, 
+        crop_percent=args.crop_percent, 
+        transform=test_transforms)
 
-    model = ConvNet(num_classes).to(device)
+    
+    trainloader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True, num_workers=1, pin_memory=True)
+    devloader = DataLoader(valid_data, batch_size=100, num_workers=1, pin_memory=True)
+    testloader = DataLoader(test_data, batch_size=100, num_workers=1, pin_memory=True)
 
-    # Loss and optimizer
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    if not os.path.isdir('results'):
+        os.mkdir('results')
 
-    # Train the model
-    total_step = len(trainloader)
-    for epoch in range(num_epochs):
-        for i, (images, labels) in enumerate(trainloader):
-            images = images.to(device)
-            labels = labels.to(device)
-            
-            # Forward pass
-            outputs = model(images)
-            loss = criterion(outputs, labels)
-            
-            # Backward and optimize
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            
-            if (i+1) % 1 == 0:
-                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(
-                    epoch+1, num_epochs, i+1, total_step, loss.item()))
+    trainer = Trainer(args)
+    trainer.train_model(trainloader, devloader)
+    
+    
 
     
