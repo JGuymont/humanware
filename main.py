f69e50f91f813eb6ac3ac2e7b7a594618d6a94da
unknown <j.guymont@gmail.com>
2019-01-23 17:17:48 -0500

    nothing

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def argparser():
     parser.add_argument('--valid_pct', type=float, default=0.2)
     parser.add_argument('--test_pct', type=float, default=0.1)
 
-    parser.add_argument('--model', type=str, choices=['SmallCNN', 'MediumCNN', 'LargeCNN'])
+    parser.add_argument('--model', type=str, choices=['SmallCNN', 'MediumCNN', 'LargeCNN', 'ResNet'])
     parser.add_argument('--num_classes', type=int, default=5)
     parser.add_argument('--n_epochs', type=int, default=100)
     parser.add_argument('--batch_size', type=int, default=128)
@@ -28,6 +28,7 @@ def argparser():
     parser.add_argument('--crop_percent', type=float, default=0.3)
     parser.add_argument('--optim', type=str, default='SGD')
     parser.add_argument('--device', type=str, default='cpu')
+    
     return parser.parse_args()
 
 if __name__ == '__main__':
@@ -70,7 +71,6 @@ if __name__ == '__main__':
         crop_percent=args.crop_percent, 
         transform=test_transforms)
 
-    
     trainloader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True, num_workers=4, pin_memory=True)
     devloader = DataLoader(valid_data, batch_size=100, num_workers=4, pin_memory=True)
     testloader = DataLoader(test_data, batch_size=100, num_workers=4, pin_memory=True)
