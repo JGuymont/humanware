9c91c7a11b09afdee81bd675434d209bb8dddf3d
Jonathan Guymont <j.guymont@gmail.com>
2019-01-24 18:16:44 -0500

    more efficient way to comput data lenght

--- trainer.py
+++ trainer.py
@@ -32,8 +32,8 @@ class Trainer:
         self.best_accuracy = 0
 
     def train_model(self, trainloader, devloader):
-        self.train_size = sum([x.shape[0] for x, _ in trainloader])
-        self.valid_size = sum([x.shape[0] for x, _ in devloader])
+        self.train_size = len(trainloader.dataset)
+        self.valid_size = len(devloader.dataset)
         for _ in tqdm(range(self.epochs)):
             self.run_epoch(trainloader, devloader)
 
