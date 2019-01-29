27584dac904a3125d15ebcd4567b9078a1716ae9
Jonathan Guymont <j.guymont@gmail.com>
2019-01-29 11:12:41 -0500

    preprocessing visualization

---
+++ utils/image.py
@@ -0,0 +1,48 @@
+import numpy as np
+from torchvision import transforms
+from PIL import Image
+
+class SVHNImage:
+    def __init__(self, metadata, image_path, crop_percent=None, transform=None):
+        self._crop_percent = crop_percent
+        self._transform = transform
+        self._image = Image.open(image_path)
+        self._min_left = min(metadata['left'])
+        self._max_left = max(metadata['left']) + max(metadata['width'])
+        self._min_top = min(metadata['top'])
+        self._max_top = max(metadata['top']) + max(metadata['height'])
+    
+    def _crop(self, image):
+        """
+        Crop an Pil image arround the bounding box that contains all
+        the digits
+        """
+        image = image.crop((self._min_left, self._min_top, self._max_left, self._max_top))
+        return image
+
+    def _crop_expand(self, image):
+        """
+        Crop an Pil image arround the bounding box that contains all
+        the digits and expand the box by 30%
+        """
+        image = image.crop((
+            (1 - self._crop_percent) * self._min_left, 
+            (1 - self._crop_percent) * self._min_top,
+            (1 + self._crop_percent) * self._max_left, 
+            (1 + self._crop_percent) * self._max_top))
+        return image
+
+    def image(self):
+        return transforms.ToTensor()(self._image)
+    
+    def bounded_image(self):
+        image = self._crop(self._image)
+        return transforms.ToTensor()(image)
+    
+    def cropped_image(self):
+        image = self._crop_expand(self._image)
+        return transforms.ToTensor()(image)
+    
+    def transformed_image(self):
+        return self._transform(self._crop_expand(self._image))
+
--- utils/visualization.py
+++ utils/visualization.py
@@ -2,17 +2,23 @@ import pickle
 from torchvision import transforms
 import matplotlib.pyplot as plt
 import numpy as np
+from torchvision import transforms
+from data import SVHNDataset
+from image import SVHNImage
 
 METADATA_PATH = './data/SVHN/train_metadata.pkl'
+IMAGES_PATH = './data/SVHN/train'
 
 def load_pickle(path):
     with open(path, 'rb') as f:
         pickle_file = pickle.load(f)
     return pickle_file
 
-def tensor_to_image(tensor):
+def tensor_to_image(tensor, save=None):
     img = transforms.ToPILImage(mode='RGB')(tensor)
     img.show()
+    if save:
+        img.save(save)
 
 def count_elements(seq) -> dict:
     hist = {}
@@ -31,16 +37,38 @@ def bar_plot(data, xtitle, title):
     plt.savefig('./figures/{}.png'.format(title))
     plt.close()
 
-def main(metadata_path):
-    metadata = load_pickle(metadata_path)
+def plot_image_transformation(metadata, data_dir, index=1):
+    transform = transforms.Compose([
+        transforms.Resize((64, 64)),
+        transforms.RandomCrop(54),
+        transforms.ToTensor(),
+    ])
+
+    image_path = '{}/{}.png'.format(data_dir, index)
+
+    image = SVHNImage(metadata[index-1]['metadata'], image_path, crop_percent=0.3, transform=transform)
+    orig_image = image.image()
+    bounded_image = image.bounded_image()
+    cropped_image = image.cropped_image()
+    transformed_image = image.transformed_image()
+
+    tensor_to_image(orig_image, save='./figures/original_image.png')
+    tensor_to_image(bounded_image, save='./figures/bounded_image.png')
+    tensor_to_image(cropped_image, save='./figures/cropped_image.png')
+    tensor_to_image(transformed_image, save='./figures/transformed_image.png')
+
+def main(metadata):
     labels = [example['metadata']['label'] for example in metadata.values()]
     lenghts = [len(label) for label in labels]
     digits = [int(digit) for label in labels for digit in label]
 
-    bar_plot(lenghts, xtitle='lenght', title='lenghts')
-    bar_plot(digits, xtitle='digit', title='digits')
+    bar_plot(lenghts, xtitle='lenght', title='hist_lenghts')
+    bar_plot(digits, xtitle='digit', title='hist_digits')
 
     
 
 if __name__ == '__main__':
-    main(METADATA_PATH)
+    metadata = load_pickle(METADATA_PATH)
+    main(metadata)
+
+    plot_image_transformation(metadata, IMAGES_PATH)
